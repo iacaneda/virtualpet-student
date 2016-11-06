f5ce76eeb1e174b5f64fa2c62e5944b097e5d527
iacaneda <iacaneda@Buffalo.edu>
2016-11-06 15:13:31 -0500

    initial commit

---
+++ petscripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+//1. (Variables) Set up three variables to store name, species, and age information about your pet.
+
+//2. (Variables) Now set the text of the paragraphs (<p></p>) set up in the About Your Pet panel to the information you set above.
+
+
+//3. (Operations & Functions) Feed your pet! Code the Feed Me button to add health points and to your pet whenever you click it.
+
+
+/*4. (Control Flow) Don't overfeed your pet! Code an alert that prompts pet owners when their pet is full (HP <=10)
+  Hint: This is also a lesson on "scope".
+*/
+
+//5. (Arrays) Train your pet! Create an array of tricks/powers your pet knows. For this portion, print to console.
+
+//6. (Arrays) Teach your pet a new trick! Add an element to your pet's tricks array. For this portion, print to console.
+
+
+//6 continued. (Arrays) Your pet is too old to learn a new trick. Remove the last trick from the array.
+
+
+/*7. (Loops) Using a for loop...
+go through the tricks array and add a random skill level for each trick;
+*/
+
+/*BONUS: Like any fun virtual pet game, your pet should be able to prove itself in
+an arena (in a less-violent Pokemon way)! The arena already displays your pet on the
+left side, a BATTLE! button, and another pet on the right. The right-side pet is your
+pet's opponent.
+You need to program the BATTLE! button's onclick function to:
+1. Determine a winner of the current battle and display it
+You need to program the New Battle button's onclick function to:
+2. Change your pet's opponent image to start a new battle
+You are given:
+var ap = your pet's attack power
+var apOpponent = your opponent's randomly generated attack power
+var opponents = array of different image sources for the opponent's image
+*/
+});
+
+
+
+
+
+/*Feel free to use any of the code you've seen today as reference for your "pass out of class".
+Your pass out of class for today is simply to add something to your GitHub Page and push changes
+to GitHub. REMEMBER: DO NOT EDIT THE FILES DIRECTLY ON GITHUB IN YOUR BROWSER! This will lead to all
+sorts of problems and merge conflicts if you do. Please either use Atom, or another text editor of your
+choice for these assignments.*/
+
+/*Note, this main function is written in its jquery form.
+  To achieve the same functionality with plain jane JS would be
+  the code featured here:
+  http://stackoverflow.com/questions/9899372/pure-javascript-equivalent-to-jquerys-ready-how-to-call-a-function-when-the
+*/
